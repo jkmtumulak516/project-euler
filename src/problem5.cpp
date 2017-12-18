ca9a3b98e8fd64a4f5d5fd20302c76d7cd0719dd
Jose Karol Tumulak <tumulakjkm@gmail.com>
2017-12-18 11:38:00 +0800

    added alternative solution to problem 5

--- src/problem5.cpp
+++ src/problem5.cpp
@@ -17,6 +17,7 @@ int main()
 
     return 0;
 }
+
 ll smallestMultiple()
 {
     ll ctr = 0;
@@ -35,4 +36,10 @@ ll smallestMultiple()
         }
         ctr++;
     }
+}
+
+// alternative
+ll smallestMultiple2()
+{
+    return 11 * 13 * 14 * 17 * 18 * 19 * 20;
 }
