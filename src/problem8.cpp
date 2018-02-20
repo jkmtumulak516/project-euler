7eab00f6ef87c2ad89057a19ea1916c5dc1f707c
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-02-20 22:57:59 +0800

    partial (optimized) solution to problem8

--- src/problem8.cpp
+++ src/problem8.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include <string>
 #include <queue>
+#include <vector>
+#include <deque>
 #include <stdio.h>
-#include <iostream>
 
 typedef long long ll;
 
@@ -17,55 +19,86 @@ const int REMAINING_SEQUENCE = TOTAL_SEQUENCE - TARGET_SEQUENCE;
 // 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
 // Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 
-int main() 
+int largestSequenceIndex(vector<int>& arr);
+int charToInt(char c);
+int charToInt(int i);
+
+int main(int argc, char *argv[]) 
 {
-    int* arr = new int[1000];
+    vector<int>* v = new vector<int>();
 
     for (int i = 0; i < TOTAL_SEQUENCE; i++)
-        arr[i] = charToInt(getchar());
+        v->push_back(charToInt(getchar()));
 
-    int index = largestSequenceIndex(arr);
+    int index = largestSequenceIndex(*v);
+
     int ceil_index = index + TARGET_SEQUENCE;
-
     int product = 1;
     
-    int (int i = index; i < ceil_index; i++) 
+    for (int i = index; i < ceil_index; i++) 
     {
-        product *= arr[i];
+        product *= v->at(i);
+        cout << i << " -> " << v->at(i) << endl;
     }
+    cout << endl;
 
-    cout << product;
+    cout << "index -> " << index << endl;
+    cout << "product -> " << product << endl;
+
+    delete v;
 
     return 0;
 }
 
-int largestSequenceIndex(int* arr)
+int largestSequenceIndex(vector<int>& v)
 {
-    queue<int> q;
+    deque<int>* q = new deque<int>(0);
     int zero_ctr = 0;
     int current_sum = 0;
     int greatest_index = 12;
+    int greatest_sum;
 
-    for (int i = 0; i < TARGET_SEQUENCE) 
+    for (int i = 0; i < TARGET_SEQUENCE; i++) 
     {
-        int num = arr[i];
-        sum 
+        int num = v[i];
+        current_sum += v[i]; 
 
         if (num == 0)
             zero_ctr++;
 
-        q.push_back(num);
+        q->push_back(num);
     }
 
-    for (int i = TARGET_SEQUENCE; i < TOTAL_SEQUENCE; i++, index++) 
+    greatest_sum = current_sum;
+
+    for (int i = TARGET_SEQUENCE; i < TOTAL_SEQUENCE; i++) 
     {
-        int num = arr[i];
-        int new_sum = num + current_sum;
+        int num = v[i];
 
+        int new_sum = (current_sum + num) - q->front();
+        int new_zero_ctr = (zero_ctr + (num != 0 ? 0 : 1)) - (q->front() != 0 ? 0 : 1);
+
+        if (new_zero_ctr == 0)
+        {
+            cout << "i: " << greatest_index << " - " << i << endl;
+            cout << "z: " << zero_ctr << " - " << new_zero_ctr << endl;
+            cout << "s: " << greatest_sum << " - " << new_sum << endl << endl;
+        }
+
+        if (new_zero_ctr == 0 && new_sum > greatest_sum)
+        {
+            greatest_index = i;
+            greatest_sum = new_sum;
+        }
+
+        current_sum = new_sum;
+        zero_ctr = new_zero_ctr;
         
-
-        if (zero_ctr)
+        q->pop_front();
+        q->push_back(num);
     }
+
+    delete q;
 
     return greatest_index - (TARGET_SEQUENCE - 1);
 }
