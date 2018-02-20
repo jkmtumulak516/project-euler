d04697b2e1409b0de7fc3f378eff242531f664a2
Jose Karol Tumulak <tumulakjkm@gmail.com>
2018-02-20 23:21:01 +0800

    problem 8 remains unsolved

--- src/problem8.cpp
+++ src/problem8.cpp
@@ -19,7 +19,6 @@ const int REMAINING_SEQUENCE = TOTAL_SEQUENCE - TARGET_SEQUENCE;
 // 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
 // Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 
-int largestSequenceIndex(vector<int>& arr);
 int charToInt(char c);
 int charToInt(int i);
 
@@ -30,77 +29,30 @@ int main(int argc, char *argv[])
     for (int i = 0; i < TOTAL_SEQUENCE; i++)
         v->push_back(charToInt(getchar()));
 
-    int index = largestSequenceIndex(*v);
+    int max_index = 0;
+    int max_product = 1;
 
-    int ceil_index = index + TARGET_SEQUENCE;
-    int product = 1;
-    
-    for (int i = index; i < ceil_index; i++) 
+    for (int i = 0; i < REMAINING_SEQUENCE; i++)
     {
-        product *= v->at(i);
-        cout << i << " -> " << v->at(i) << endl;
-    }
-    cout << endl;
+        int sequence_ceil = i + TARGET_SEQUENCE;
+        int product = 1;
 
-    cout << "index -> " << index << endl;
-    cout << "product -> " << product << endl;
+        cout << i << " -> " << sequence_ceil << endl;
+        for (int j = i; j < sequence_ceil; j++)
+            product *= v->at(j);
+
+        if (product > max_product)
+        {
+            max_index = i;
+            max_product = product;
+        } 
+    }
+
+    cout << endl << max_product << " - " << max_index << endl;
 
     delete v;
 
     return 0;
-}
-
-int largestSequenceIndex(vector<int>& v)
-{
-    deque<int>* q = new deque<int>(0);
-    int zero_ctr = 0;
-    int current_sum = 0;
-    int greatest_index = 12;
-    int greatest_sum;
-
-    for (int i = 0; i < TARGET_SEQUENCE; i++) 
-    {
-        int num = v[i];
-        current_sum += v[i]; 
-
-        if (num == 0)
-            zero_ctr++;
-
-        q->push_back(num);
-    }
-
-    greatest_sum = current_sum;
-
-    for (int i = TARGET_SEQUENCE; i < TOTAL_SEQUENCE; i++) 
-    {
-        int num = v[i];
-
-        int new_sum = (current_sum + num) - q->front();
-        int new_zero_ctr = (zero_ctr + (num != 0 ? 0 : 1)) - (q->front() != 0 ? 0 : 1);
-
-        if (new_zero_ctr == 0)
-        {
-            cout << "i: " << greatest_index << " - " << i << endl;
-            cout << "z: " << zero_ctr << " - " << new_zero_ctr << endl;
-            cout << "s: " << greatest_sum << " - " << new_sum << endl << endl;
-        }
-
-        if (new_zero_ctr == 0 && new_sum > greatest_sum)
-        {
-            greatest_index = i;
-            greatest_sum = new_sum;
-        }
-
-        current_sum = new_sum;
-        zero_ctr = new_zero_ctr;
-        
-        q->pop_front();
-        q->push_back(num);
-    }
-
-    delete q;
-
-    return greatest_index - (TARGET_SEQUENCE - 1);
 }
 
 int charToInt(char c) 
